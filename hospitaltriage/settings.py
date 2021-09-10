375f44d47e5ef2f2115874f80123458840f36745
Nicholas R Johnson <njohnson@nicholasrjohnson.com>
2021-09-10 19:52:44 -0400

    Fixed error in whitenoise middleware. typo

--- hospitaltriage/settings.py
+++ hospitaltriage/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middelware.WhiteNoiseMiddleWare',
+    'whitenoise.middelware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hospitaltriage.urls'
